3485c07ea4fce10496a2bf83d71b521bf1a8b55f
Jon Ho <housuilun@yahoo.com>
2014-12-11 08:36:34 -0800

    try again

--- config/initializers/dashing.rb
+++ config/initializers/dashing.rb
@@ -5,7 +5,7 @@ Dashing.configure do |config|
 
   # Redis credentials.
   # See https://devcenter.heroku.com/articles/redistogo to configure redis for heroku.
-  config.redis_host     = URI.parse("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/").host #"redistogo"
+  config.redis_host     = URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).host #"redistogo"
   config.redis_port     = URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).port #"9557"
   config.redis_password = URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).password #"14419eb348fc2f725019d5a1a0d9a463" 
   # config.redis_timeout  = 3
