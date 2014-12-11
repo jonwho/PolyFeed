71c20d87a17ce2c355e43ba842a44a78c919239f
Jon Ho <housuilun@yahoo.com>
2014-12-11 08:34:01 -0800

    test again for deploy

--- config/initializers/dashing.rb
+++ config/initializers/dashing.rb
@@ -5,9 +5,9 @@ Dashing.configure do |config|
 
   # Redis credentials.
   # See https://devcenter.heroku.com/articles/redistogo to configure redis for heroku.
-  config.redis_host     = "redistogo" #URI.parse().host
-  config.redis_port     = "9557" #URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).port
-  config.redis_password = "14419eb348fc2f725019d5a1a0d9a463" #URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).password
+  config.redis_host     = URI.parse("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/").host #"redistogo"
+  config.redis_port     = URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).port #"9557"
+  config.redis_password = URI.parse(URI.encode("redis://redistogo:14419eb348fc2f725019d5a1a0d9a463@dab.redistogo.com:9557/")).password #"14419eb348fc2f725019d5a1a0d9a463" 
   # config.redis_timeout  = 3
 
   # Redis namespace when pushing new data.
